e1781bb16732ed5fd56bb349b252e0aa652528d5
Zynj <adnexuscentral@gmail.com>
2020-09-04 08:27:57 +0200

    Added 'history' and 'worldEntries' for scripting interface testing

--- notes.js
+++ notes.js
@@ -2,6 +2,8 @@
 // Keys is a comma-delimited string of words and by default can be assigned a max of 50 characters, but via the associated functions there is no limit.
 // Entries by standard takes 500 characters, but again; scripts bypass any such restrictions although it's not recommended to do so.
 worldEntries = [{"id": 0.012345689, "keys": "key, words", "entry": "your custom entry"}]
+const history = [{"text": "This is the history", "type": "continue"},{"text": "These is the history", "type": "continue"}]
+const worldEntries = [{"keys": "These, are, your, keys", "entry": "This is an entry."}, {"keys": "This, is, your, key", "entry": "This is an entry."}]
 
 addWorldEntry(keys, entry)
 removeWorldEntry(index)
