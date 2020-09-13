29ee301059dd6fc47c6443da341a2d8556df37a2
Zynj <adnexusentral@gmail.com>
2020-09-13 14:34:32 +0200

    Fixed some reference errors

--- LocalEnvironment/interface.mjs
+++ LocalEnvironment/interface.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs' // This is required for writing back into the file on the end of the turn.
 const persistentData = JSON.parse(fs.readFileSync('persistentData.json')) // Stored in a JSON file that's written to on the end of script execution after temporary changes have been made.
 import { updateWorldEntry, addWorldEntry } from './nativeFunctions.mjs' // Defined as const values in nativeFunctions.mjs
-let {worldEntries, state} = persistentData // Reassignable
+export let {worldEntries, state} = persistentData // Reassignable
 const {history, memory} = persistentData // None reassignable
 const updateState = () => {const formatData = {"state": {"memory":{"context": state.memory.context,"frontMemory": state.memory.frontMemory,"authorsNote": state.memory.authorsNote}},"memory": memory,"worldEntries": worldEntries,"history": history}; fs.writeFile("persistentData.json", JSON.stringify(formatData, null, 2), (err) => {if (err) {console.error(err);return;};console.log("State Updated in persistentData.json");});}
 
@@ -10,7 +10,9 @@ const text = "Test String"
 const modifier = (text) =>
 {
 
-    state.memory.authorsNote = "1239112313231"
+    state.memory.authorsNote = "12391121231231313231"
+    addWorldEntry("new, keys", "new entry")
+    updateWorldEntry(0, "HellO?", "Can you hear me?")
     updateState(); // Store the persistent state in-between executions.
     return {text}
 }
