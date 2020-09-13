dd457304680e6065abccd263d709ef515601b1c7
Zynj <adnexusentral@gmail.com>
2020-09-13 17:04:38 +0200

    removeWorldEntry and background.mjs

--- LocalEnvironment/interface.mjs
+++ LocalEnvironment/interface.mjs
@@ -1,7 +1,8 @@
 // Ignore this section and don't include it in your actual scripts. It's just here to emulate the native functionality of AI Dungeon's scripting.
 import fs from 'fs' // This is required for writing back into the file on the end of the turn.
 const persistentData = JSON.parse(fs.readFileSync('persistentData.json')) // Stored in a JSON file that's written to on the end of script execution after temporary changes have been made.
-import { updateWorldEntry, addWorldEntry, processInput } from './nativeFunctions.mjs' // Defined as const values in nativeFunctions.mjs
+import { updateWorldEntry, addWorldEntry, removeWorldEntry} from './nativeFunctions.mjs' // Defined as const values in nativeFunctions.mjs
+import { processInput } from './background.mjs' // Imports functions used for pre-processing the input to e.g determine its type in history.
 export let {worldEntries, state} = persistentData // Reassignable
 const {history, memory} = persistentData // None reassignable
 const updateState = () => {const formatData = {"state": {"memory":{"context": state.memory.context,"frontMemory": state.memory.frontMemory,"authorsNote": state.memory.authorsNote}},"memory": memory,"worldEntries": worldEntries,"history": history}; fs.writeFile("persistentData.json", JSON.stringify(formatData, null, 2), (err) => {if (err) {console.error(err);return;};console.log("State Updated in persistentData.json");});}
@@ -23,7 +24,6 @@ const modifier = (text) =>
     state.memory.authorsNote = "12391121231231313231"
     addWorldEntry("new, keys", "new entry")
     updateWorldEntry(0, "HellO?", "Can you hear me?")
-    
     console.log(text)
     return {text}
 }
