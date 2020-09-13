ab7178eee99046ecc3ca0709426824b6ee444641
Zynj <adnexusentral@gmail.com>
2020-09-13 17:27:29 +0200

    Format Comments and code.

--- LocalEnvironment/background.mjs
+++ LocalEnvironment/background.mjs
@@ -1,7 +1,11 @@
 // Holds background function and processing that shouldn't be utilized in your scripts, but does affect how they are treated.
 // e.g the various input types has various pre-processing that you should be aware of, but it's not particularily important for testing purposes
 // other than to determine the 'type' key in history.
+import fs from 'fs'
+import {worldEntries, state, history, memory} from './interface.mjs'
 
 const prefix = '/' // This is the command prefix used for written commands in AI Dungeon.
 export const processInput = () => {const parameters = process.argv.slice(2); let type, text; if (parameters.length > 1) {type = parameters[0].slice(prefix.length); text = parameters[1]} else {type = 'continue'; text = parameters[0]} return {"text": text, "type": type}}
+export const updateHistory = (input) => history.push(input)
+export const updateState = () => {const formatData = {"state": {"memory":{"context": state.memory.context,"frontMemory": state.memory.frontMemory,"authorsNote": state.memory.authorsNote}},"memory": memory,"worldEntries": worldEntries,"history": history}; fs.writeFile("persistentData.json", JSON.stringify(formatData, null, 2), (err) => {if (err) {console.error(err);return;};console.log("State Updated in persistentData.json");});}
 //const getForm = (word) => {const conversions = {'I': 'you','me': 'you','myself': 'yourself','mine': 'yours','my': 'your',}; if (conversions.hasOwnProperty(word)) {return conversions[word]} return word}
--- LocalEnvironment/interface.mjs
+++ LocalEnvironment/interface.mjs
@@ -1,22 +1,22 @@
-// Ignore this section and don't include it in your actual scripts. It's just here to emulate the native functionality of AI Dungeon's scripting.
-import fs from 'fs' // This is required for writing back into the file on the end of the turn.
-const persistentData = JSON.parse(fs.readFileSync('persistentData.json')) // Stored in a JSON file that's written to on the end of script execution after temporary changes have been made.
-import { updateWorldEntry, addWorldEntry, removeWorldEntry} from './nativeFunctions.mjs' // Defined as const values in nativeFunctions.mjs
-import { processInput } from './background.mjs' // Imports functions used for pre-processing the input to e.g determine its type in history.
-export let {worldEntries, state} = persistentData // Reassignable
-const {history, memory} = persistentData // None reassignable
-const updateState = () => {const formatData = {"state": {"memory":{"context": state.memory.context,"frontMemory": state.memory.frontMemory,"authorsNote": state.memory.authorsNote}},"memory": memory,"worldEntries": worldEntries,"history": history}; fs.writeFile("persistentData.json", JSON.stringify(formatData, null, 2), (err) => {if (err) {console.error(err);return;};console.log("State Updated in persistentData.json");});}
-const updateHistory = (input) => history.push(input)
-const input = processInput(); const text = input["text"];
-
-
-// Don't include the above section in your scripts. It's just here to emulate the native functionality of AI Dungeon's scripting.
-
-// Execute the script through the command line, the first argument becomes 'text'.
+// Execute the script through the command line, the first argument becomes 'text' use /do, /say and /story before your string to assign the type.
 // e.g node interface.mjs "Hello World!"
 // 'text' then becomes "Hello World!"
 // If you intend to repeatedly test the same string, then just replace the const text declaration.
 // Native functions are found in nativeFunctions.mjs
+
+// Ignore this section and don't include it in your actual scripts. It's just here to emulate the native functionality of AI Dungeon's scripting.
+import fs from 'fs' // This is required for writing back into the file on the end of the turn.
+const persistentData = JSON.parse(fs.readFileSync('persistentData.json')) // Stored in a JSON file that's written to on the end of script execution after temporary changes have been made.
+// Import background functionalities for local testing. None of these are relevant for your actual scripts.
+import { processInput, updateHistory, updateState} from './background.mjs' // Imports functions used for pre-processing the input to e.g determine its type in history.
+const input = processInput(); const text = input["text"]; // Create the 'text' object, it's later passed to history after processing.
+// Import the 'native' (emulated) functions of AI Dungeon, these can all be used in the same manner within AI Dungeon itself.
+import { updateWorldEntry, addWorldEntry, removeWorldEntry} from './nativeFunctions.mjs'
+// This imports the persistent values as an emulation of 'state'/game follows the same structure as native AI Dungeon. e.g state.memory.authorsNote
+export let {worldEntries, state} = persistentData // Reassignable
+export const {history, memory} = persistentData // None reassignable
+// Don't include the above section in your scripts. It's just here to emulate the native functionality of AI Dungeon's scripting.
+
 
 const modifier = (text) =>
 {
@@ -24,8 +24,15 @@ const modifier = (text) =>
     state.memory.authorsNote = "12391121231231313231"
     addWorldEntry("new, keys", "new entry")
     updateWorldEntry(0, "HellO?", "Can you hear me?")
-    console.log(text)
+    console.log()
     return {text}
 }
 modifier(text) // Don't change this line.
-updateHistory(input); updateState(); // Remove this line to prevent stored/persistent changes.
+
+
+
+// Do not include the below section in your scripts.
+// The below code handles persistence/history updates after each execution.
+// Omit this section if you don't wish or need to utilize this functionality.
+updateHistory(input); // Adds the text and
+updateState(); // Remove this line to prevent stored/persistent changes.
