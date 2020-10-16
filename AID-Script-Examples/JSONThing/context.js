2e9ce0f3d8cec8706b3fa95d28665d1b7d724bda
Zynj <adnexusentral@gmail.com>
2020-10-16 07:19:58 +0200

    Contextual properties and whitelist

--- AID-Script-Examples/JSONThing/context.js
+++ AID-Script-Examples/JSONThing/context.js
@@ -8,6 +8,10 @@ const getKey = (keys, obj) => { return keys.split('.').reduce((a, b) => { if (ty
 const getHistoryString = (turns) => history.slice(turns).map(element => element["text"]).join(' ') // Returns a single string of the text.
 const getMemory = (text) => { return info.memoryLength ? text.slice(0, info.memoryLength) : '' }
 const getContext = (text) => { return info.memoryLength ? text.slice(info.memoryLength) : text }
+// Whitelisted properties defined in an entry keyworded 'whitelist' will always display above the last mention of root.
+const getWhitelist = () => worldEntries.filter(entry => entry["keys"].includes('whitelist'))[0]["entry"].split(',').map(element => element.trim())
+// Contextual properties will be referenced in frontMemory when prompted.
+const getContextualProperties = (text) => { return worldEntries.filter(entry => entry["keys"].includes('synonyms.') && entry["entry"].split(',').some(key=> text.includes(key))).map(element => element["keys"].split('.').pop());}
 // Assign the property defined in the wEntry's keys with its entry value.
 const setProperty = (keys, value, obj) => { const property = keys.split('.').pop(); const path = keys.split('.').slice(0, -1).join('.'); getKey(path, obj)[property] = value.includes(',') ? value.split(',').map(element => element.trim()) : value }
 // Loop through worldEntries and assign the properties within state.data
@@ -22,10 +26,12 @@ const modifier = (text) => {
     let memoryLines = contextMemory.split('\n');
 
     // Loop through the previously defined properties in reverse order, then reverse again. Flip flop, *dab*.
-    for (const data in dataStorage) { lines.reverse().some(line => {if (!line.includes('[') && line.toLowerCase().includes(data)) {lines.splice(lines.indexOf(line) + 1, 0, `[${JSON.stringify(dataStorage[data])}]`); return true}}); lines.reverse(); }
+    for (const data in dataStorage) { lines.reverse().some(line => {if (!line.includes('[') && line.toLowerCase().includes(data)) {lines.splice(lines.indexOf(line) + 1, 0, `[${JSON.stringify(dataStorage[data], [getWhitelist(), getContextualProperties(getHistoryString(-1))].flat())}]`); return true}}); lines.reverse(); }
+    getHistoryString(-1).split(' ').reverse().some(word => { for (const data in dataStorage) {if (word.toLowerCase().includes(data)) { const whitelist = getContextualProperties(getHistoryString(-1)); whitelist.length >= 1 ? lines.splice(lines.length, 0, `\n> [${JSON.stringify(dataStorage[data], ['character', whitelist].flat())}]\n`) : {}; return true}}})
+
     let combinedLines = lines.join('\n').slice(-(info.maxChars - info.memoryLength))
     // Lazy patchwork to """fix""" linebreak spam.
-    while (combinedLines.includes('\n\n')) { combinedLines = combinedLines.replace('\n\n', '\n') }
+    //while (combinedLines.includes('\n\n')) { combinedLines = combinedLines.replace('\n\n', '\n') }
     const finalText = [contextMemory, combinedLines].join("")
     return { text: finalText }
 }
