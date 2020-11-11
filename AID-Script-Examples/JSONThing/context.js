88350736a86a1ed1a6f0d462eb8ba778e7ca3048
Zynj <adnexusentral@gmail.com>
2020-11-11 19:45:58 +0100

    Fix: Ensure that child copy has child root as synonym.

--- AID-Script-Examples/JSONThing/context.js
+++ AID-Script-Examples/JSONThing/context.js
@@ -18,14 +18,14 @@ const modifier = (text) => {
         const localWhitelist = getContextualProperties(getHistoryString(-1)).flat();
         const localReplacer = (name, val) => { if (localWhitelist.some(element => element.includes(name)) && val) { return Array.isArray(val) ? val.join(', ') : val } else { return undefined } };
         // Process child references before inserting the JSON lines.
-        
+
         // Reverse lines to hit last match instead of first.
         lines.reverse()
         for (const data in dataStorage) {
             if (dataStorage[data].hasOwnProperty("child")) {
                 let indexPos = -1;
                 let finalParent; // Measure the child properties against each other, the final/most recent mention becomes set as root.
-
+                let finalParentName;
                 // Successful detections, especially soft-copies of unique root, results in duplicate JSON line insertions.
                 // Comparing properties is insufficient as generic child inheritance does not equal child - e.g synonyms polution.
                 // Previous solution implemented a 'skip' property to 'finalParent' on assignment, but did not hit 'most recent' (first .some() match determined insertion).
@@ -43,6 +43,7 @@ const modifier = (text) => {
                             if (element.includes('[')) {
                                 toCopy = element.replace(`[`, '').replace(`]`, '');
                                 finalParent = dataStorage[toCopy];
+                                finalParentName = searchFor;
                                 break;
                             }
                             // If it's not a copy of an existing root, grab the entire branch from root.child[...]
@@ -55,7 +56,7 @@ const modifier = (text) => {
 
                 if (finalParent) {
                     // Do a risque move and merge synonyms together. 
-                    finalParent['synonyms'] ? finalParent['synonyms'] += data : finalParent['synonyms'] = data;
+                    finalParent['synonyms'] ? finalParent['synonyms'] += data + ', ' + finalParentName : finalParent['synonyms'] = data + ', ' + finalParentName;
                     if (dataStorage['synonyms'].hasOwnProperty(data)) { finalParent['synonyms'] += ', ' + dataStorage['synonyms'][data]; }
                     if (dataStorage[data].hasOwnProperty('synonyms')) { finalParent['synonyms'] += ', ' + dataStorage[data]['synonyms']; }
                     // Merge the root, e.g tavern with tavern.children.nordfall; non-unique properties are over-written.
@@ -64,27 +65,28 @@ const modifier = (text) => {
             }
 
             // Correctly (🤔) find the most recent match between synonyms and child copies.
-            let finalIndex = -1; let finalWord; const checkWords = [...[data], ...getRootSynonyms(data)]; checkWords.forEach(word => { const index = modifiedContext.lastIndexOf(word.toLowerCase()); if (index > finalIndex) {finalIndex = index; finalWord = word;}})
             //console.log(`Check: ${checkWords}| Final: ${finalWord}`)
             // Insertion of JSON lines at last match.
-            lines.some(line => {   
+            let finalIndex = -1; let finalWord; const checkWords = [...[data], ...getRootSynonyms(data)]; checkWords.forEach(word => { const index = modifiedContext.lastIndexOf(word.toLowerCase()); if (index > finalIndex) { finalIndex = index; finalWord = word; } })
+            lines.some(line => {
                 const regEx = new RegExp('\\b' + finalWord, 'gi');
                 if (!line.includes('[') && regEx.test(line)) {
                     // Stringify the dataStorage by displaying the whitelisted/contextual properties.
                     let string = JSON.stringify(dataStorage[data], globalReplacer).replace(/\\/g, '');
-                    if (state.settings["filter"]) {string = string.replace(/"|{|}/g, '')}
+                    if (state.settings["filter"]) { string = string.replace(/"|{|}/g, '') }
                     // If it's not an empty JSON [{}] <-- 4 chars and none of the lines currently include the JSON (e.g when trying to display from unique and child)
                     // Could potentially check for string duplicates per line, but order is an issue... compare indexes?
-                    if (string.length > 4 && !lines.some(line => line.includes(string))) { lines.splice(lines.indexOf(line) + 1, 0, `[${string}]`); return true;}
+                    if (string.length > 4 && !lines.some(line => line.includes(string))) { lines.splice(lines.indexOf(line) + 1, 0, `[${string}]`); return true; }
                 }
             });
+
         }
         // Reverse the line back into normal order.
-        lines.reverse(); 
+        lines.reverse();
 
         const JSONLines = lines.filter(line => line.startsWith('['))
         const JSONString = JSONLines.join('\n');
-        if (state.settings["entriesFromJSON"]) {const normalWorldEntries = worldEntries.filter(element => { if (!element["keys"].includes('.') && (!element["keys"].includes(whitelistPath))) { return true } }); normalWorldEntries.forEach(element => element["keys"].split(',').some(keyword => { if (JSONString.toLowerCase().includes(keyword.toLowerCase()) && !text.includes(element["entry"])) { if (info.memoryLength + contextMemoryLength + element["entry"].length <= info.maxChars / 2) { memoryLines.splice(-1, 0, element["entry"]); contextMemoryLength += element["entry"].length + 1; return true; } } })) }
+        if (state.settings["entriesFromJSON"]) { const normalWorldEntries = worldEntries.filter(element => { if (!element["keys"].includes('.') && (!element["keys"].includes(whitelistPath))) { return true } }); normalWorldEntries.forEach(element => element["keys"].split(',').some(keyword => { if (JSONString.toLowerCase().includes(keyword.toLowerCase()) && !text.includes(element["entry"])) { if (info.memoryLength + contextMemoryLength + element["entry"].length <= info.maxChars / 2) { memoryLines.splice(-1, 0, element["entry"]); contextMemoryLength += element["entry"].length + 1; return true; } } })) }
         // Uncommenting this line adds 'actionized' properties to the fore-front of context to have them directly affect the outcome. Should not be necessary, but may improve outcomes in certain situations. Feel free to experiment with it. If a property is not whitelisted, it's required to have a synonyms. definition to show.
         //getHistoryString(-1).split(' ').reverse().some(word => { for (const data in dataStorage) {if (word.toLowerCase().includes(data)) { JSON.stringify(dataStorage[data], localReplacer).length > 2 ? lines.splice(lines.length, 0, `\n> [${JSON.stringify(dataStorage[data], localReplacer)}]\n`) : {} ; return true}}})
     }
